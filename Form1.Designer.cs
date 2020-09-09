5b121be867b20c3dedf17641bdc39aff8d366549
Adrian <agarciat@cifpfbmoll.eu>
2020-09-10 01:10:12 +0200

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -145,6 +145,7 @@
 			// 
 			// button2
 			// 
+			this.button2.Enabled = false;
 			this.button2.Location = new System.Drawing.Point(341, 415);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
